3233a8070c3924e621e00a047695c6b295de39e5
William Earley <william@earley.org.uk>
2019-09-21 14:39:57 +0100

    bit of refactoring

--- Main.hs
+++ Main.hs
@@ -7,28 +7,32 @@ import Stream (sget)
 maxVs :: Double -> (Double,Double)
 maxVs sm = maximise rate 0 smax
   where smax   = clipper 0 (8/9) sm
-        rate s = (sqrt s) * (relRate s $ cbrt (1-s/sm))
         clip   = clipper' 0 1 0
         cbrt x = (clip x) ** (1/3)
+        rate s = (sqrt s) * (relRate s $ cbrt (1-s/sm))
 
 relRate :: Double -> Double -> Double
-relRate vs 1 = sqrt (1-vs)
-relRate vs v0 = let (_,_,r') = sget ode v0 in r'
+relRate vs 1  = sqrt (1 - vs)
+relRate vs v0 = r0
   where
-    ode = dsolve' (f v0 vs) 1 (0, sqrt(1-vs), 0)
-    f v0 vs v (u,g,r)
+    ugr1 = (0, sqrt (1 - vs), 0)
+    ode = dsolve' ugr' 1 ugr1
+    (u0, g0, r0) = sget ode v0
+
+    ugr' v (u, g, r)
       | v <= v0   = go nu''
       | otherwise = go nu'
       where
-        x = (1 - v0/v)^3
-        w = 1 - (v0/v)^3
-        m = 1 - v0^3
+        x = (1 -  v0/v)^3 + 3*u
+        w =  1 - (v0/v)^3
+        m =  1 -  v0   ^3
 
-        nu' = 0.5 * v * vs * (x + 3*u) / (m - v*v*vs*w)
-        nu'' = 1.5 * v * vs * u / m
-        r' = -3 * v*v * g / m
+        nu'  = 0.5 * v * vs * x / (m - v*v*vs*w)
+        nu'' = 1.5 * v * vs * u /  m
+        r'   =  -3 * v * v  * g /  m
 
         go nu' = (-(1+u) * nu', g * nu', r')
 
 main :: IO ()
-main = mapM_ print $ [(sm,maxVs sm) | sm <- ([0,0.02..1] ++ [2..])]
+-- main = mapM_ print $ [(sm,maxVs sm) | sm <- ([0,0.02..1] ++ [2..])]
+main = mapM_ print $ [(sm,maxVs sm) | sm <- map exp [-20,-19.8..20]]
--- Vector.hs
+++ Vector.hs
@@ -1,57 +1,14 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
 module Vector where
 
 class Vector v where
     vconst :: Double -> v
-    vmap :: (Double -> Double) -> v -> v
+    vmap   :: (Double -> Double) -> v -> v
     vmap f = vzip (const f) vzero
-    vzip :: (Double -> Double -> Double) -> v -> v -> v
-    vlen :: v -> Int
-    vfold :: (Double -> Double -> Double) -> Double -> v -> Double
-
-instance Vector Double where
-    vconst = id
-    vmap = id
-    vzip = id
-    vlen = const 1
-    vfold = id
-
-instance Vector u => Vector [u] where
-    vconst = repeat . vconst
-    vmap = map . vmap
-    vzip = zipWith . vzip
-    vlen = sum . map vlen
-    vfold f b = foldr f b . map (vfold f b)
-
-instance Vector () where
-    vconst _ = ()
-    vmap _ _ = ()
-    vzip _ _ _ = ()
-    vlen _ = 0
-    vfold _ b _ = b
-
-instance (Vector u, Vector v) => Vector (u, v) where
-    vconst c = (vconst c, vconst c)
-    vmap f (u,v) = (vmap f u, vmap f v)
-    vzip f (u1,v1) (u2,v2) = (vzip f u1 u2, vzip f v1 v2)
-    vlen (u,v) = vlen u + vlen v
-    vfold f b (u,v) = ff u . ff v $ b
-      where ff u = f (vfold f b u)
-
-instance (Vector u, Vector v, Vector w) => Vector (u, v, w) where
-    vconst c = (vconst c, vconst c, vconst c)
-    vmap f (u,v,w) = (vmap f u, vmap f v, vmap f w)
-    vzip f (u1,v1,w1) (u2,v2,w2) = (vzip f u1 u2, vzip f v1 v2, vzip f w1 w2)
-    vlen (u,v,w) = vlen u + vlen v + vlen w
-    vfold f b (u,v,w) = ff u . ff v . ff w $ b
-      where ff u = f (vfold f b u)
-
-instance (Vector u, Vector v, Vector w, Vector x) => Vector (u, v, w, x) where
-    vconst c = (vconst c, vconst c, vconst c, vconst c)
-    vmap f (u,v,w,x) = (vmap f u, vmap f v, vmap f w, vmap f x)
-    vzip f (u1,v1,w1,x1) (u2,v2,w2,x2) = (vzip f u1 u2, vzip f v1 v2, vzip f w1 w2, vzip f x1 x2)
-    vlen (u,v,w,x) = vlen u + vlen v + vlen w + vlen x
-    vfold f b (u,v,w,x) = ff u . ff v . ff w . ff x $ b
-      where ff u = f (vfold f b u)
+    vzip   :: (Double -> Double -> Double) -> v -> v -> v
+    vlen   :: v -> Int
+    vfold  :: (Double -> Double -> Double) -> Double -> v -> Double
 
 vzero :: Vector v => v
 vzero = vconst 0
@@ -78,8 +35,8 @@ vmean :: Vector v => v -> Double
 vmean v = vtot v / fromIntegral (vlen v)
 
 vsum :: Vector v => [v] -> v
-vsum [] = vzero
-vsum [x] = x
+vsum []     = vzero
+vsum [x]    = x
 vsum (x:xs) = vplus x (vsum xs)
 
 vlc :: Vector v => [(Double,v)] -> v
@@ -90,3 +47,143 @@ vlc' [x]    (v:vs) = vscale x v
 vlc' (x:xs) [v]    = vscale x v
 vlc' (x:xs) (v:vs) = vplus (vscale x v) (vlc' xs vs)
 vlc' _      _      = vzero
+
+--- instances
+
+instance Vector Double where
+    vconst = id
+    vmap   = id
+    vzip   = id
+    vlen   = const 1
+    vfold  = id
+
+instance Vector u => Vector [u] where
+    vconst    = repeat . vconst
+    vmap      = map . vmap
+    vzip      = zipWith . vzip
+    vlen      = sum . map vlen
+    vfold f b = foldr f b . map (vfold f b)
+
+instance Vector () where
+    vconst _     = ()
+    vmap   _ _   = ()
+    vzip   _ _ _ = ()
+    vlen   _     = 0
+    vfold  _ b _ = b
+
+--- tuples
+
+instance (Vector u, Vector v)
+         => Vector (u, v) where
+
+    vconst c =
+        let v = vconst c
+        in (v, v)
+
+    vmap f (u,v) =
+        let g = vmap f
+        in (g u, g v)
+
+    vzip f (u1,v1) (u2,v2) =
+        let (!) = vzip f
+        in (u1!u2, v1!v2)
+
+    vlen (u,v) =
+        let g = vlen
+        in g u + g v
+
+    vfold f b (u,v) =
+        let ff u = f (vfold f b u)
+        in ff u . ff v $ b
+
+instance (Vector u, Vector v, Vector w)
+         => Vector (u, v, w) where
+
+    vconst c =
+        let v = vconst c
+        in (v, v, v)
+
+    vmap f (u,v,w) =
+        let g = vmap f
+        in (g u, g v, g w)
+
+    vzip f (u1,v1,w1) (u2,v2,w2) =
+        let (!) = vzip f
+        in (u1!u2, v1!v2, w1!w2)
+
+    vlen (u,v,w) =
+        let g = vlen
+        in g u + g v + g w
+
+    vfold f b (u,v,w) =
+        let ff u = f (vfold f b u)
+        in ff u . ff v . ff w $ b
+
+instance (Vector u, Vector v, Vector w, Vector x)
+         => Vector (u, v, w, x) where
+
+    vconst c =
+        let v = vconst c
+        in (v, v, v, v)
+
+    vmap f (u,v,w,x) =
+        let g = vmap f
+        in (g u, g v, g w, g x)
+
+    vzip f (u1,v1,w1,x1) (u2,v2,w2,x2) =
+        let (!) = vzip f
+        in (u1!u2, v1!v2, w1!w2, x1!x2)
+
+    vlen (u,v,w,x) =
+        let g = vlen
+        in g u + g v + g w + g x
+
+    vfold f b (u,v,w,x) =
+        let ff u = f (vfold f b u)
+        in ff u . ff v . ff w . ff x $ b
+
+instance (Vector u, Vector v, Vector w, Vector x, Vector y)
+         => Vector (u, v, w, x, y) where
+
+    vconst c =
+        let v = vconst c
+        in (v, v, v, v, v)
+
+    vmap f (u,v,w,x,y) =
+        let g = vmap f
+        in (g u, g v, g w, g x, g y)
+
+    vzip f (u1,v1,w1,x1,y1) (u2,v2,w2,x2,y2) =
+        let (!) = vzip f
+        in (u1!u2, v1!v2, w1!w2, x1!x2, y1!y2)
+
+    vlen (u,v,w,x,y) =
+        let g = vlen
+        in g u + g v + g w + g x + g y
+
+    vfold f b (u,v,w,x,y) =
+        let ff u = f (vfold f b u)
+        in ff u . ff v . ff w . ff x . ff y $ b
+
+instance (Vector u, Vector v, Vector w, Vector x, Vector y, Vector z)
+         => Vector (u, v, w, x, y, z) where
+
+    vconst c =
+        let v = vconst c
+        in (v, v, v, v, v, v)
+
+    vmap f (u,v,w,x,y,z) =
+        let g = vmap f
+        in (g u, g v, g w, g x, g y, g z)
+
+    vzip f (u1,v1,w1,x1,y1,z1) (u2,v2,w2,x2,y2,z2) =
+        let (!) = vzip f
+        in (u1!u2, v1!v2, w1!w2, x1!x2, y1!y2, z1!z2)
+
+    vlen (u,v,w,x,y,z) =
+        let g = vlen
+        in g u + g v + g w + g x + g y + g z
+
+    vfold f b (u,v,w,x,y,z) =
+        let ff u = f (vfold f b u)
+        in ff u . ff v . ff w . ff x . ff y . ff z $ b
